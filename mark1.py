c2e0b938252fbab2106391cac7678f257d4f38d0
Nathan Merritt <gnm@the-merritts.net>
2013-01-08 20:27:25 -0500

    initial commit, added default mark 1 bot

---
+++ mark1.py
@@ -0,0 +1,24 @@
+import sys
+
+class MyBot(object):
+
+        def run(self):
+                while not sys.stdin.closed:
+                        try:
+                                rawline = sys.stdin.readline()
+                                if len(rawline) == 0:
+                                        break
+                                line = rawline.strip()
+                                parts = line.split()
+                                if parts[0] == 'go':
+                                        sys.stdout.write('call 0')
+                                        sys.stdout.flush()
+                        except EOFError:
+                                return
+
+        def __init__(self):
+                pass
+
+if __name__ == '__main__':
+        bot = MyBot()
+        bot.run()
