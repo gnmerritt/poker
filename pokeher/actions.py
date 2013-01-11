bc3156ea8c0d55e80072b37502746b49905f57f9
Nathan Merritt <gnm@the-merritts.net>
2013-01-10 23:30:16 -0500

    stubbed out action classes

---
+++ pokeher/actions.py
@@ -0,0 +1,15 @@
+class Action:
+    """Base class for actions our bot can take"""
+    pass
+
+class Call(Action):
+    pass
+
+class Raise(Action):
+    pass
+
+class Fold(Action):
+    pass
+
+class Check(Action):
+    pass
