f7427a9c414db38ffacd8486ba2ecbc378605e49
Nathan Merritt <gnm@the-merritts.net>
2013-01-13 20:32:57 -0500

    I am bad at typing

--- test/test_theaigame_bot.py
+++ test/test_theaigame_bot.py
@@ -4,7 +4,7 @@ from pokeher.theaigame_bot import TheAiGameBot
 
 class TheAiGameBotTest(unittest.TestCase):
     """Tests instantiating the bot and feeding it a couple lines"""
-    def test_both_instantiation(self):
+    def test_bot_instantiation(self):
         # TODO: use Mock and actually test stderr and stdout
         bot = TheAiGameBot(sys.stdout, sys.stderr)
         self.assertTrue(bot)
