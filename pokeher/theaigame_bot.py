9e0f5e773b0570523ee747b5b8ba7aa65f190965
Nathan Merritt <gnm@the-merritts.net>
2015-04-16 20:41:42 -0400

    made sure crash log goes to stderr

--- pokeher/theaigame_bot.py
+++ pokeher/theaigame_bot.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
-
+from __future__ import print_function
 import sys
 sys.path.append('/Users/nathan/sources/poker/') ## TODO fix
 try:
     from wiring import IOPokerBot
 except ImportError as e:
     import os, platform
-    print "Couldn't load bot on os={os}, sys={s}, rel={r}, e={e}" \
-      .format(os=os.name, s=platform.system(), r=platform.release(), e=e)
+    error = "Couldn't load bot on os={os}, sys={s}, rel={r}, e={e}" \
+          .format(os=os.name, s=platform.system(), r=platform.release(), e=e)
+    print(error, file=sys.stderr)
     sys.exit()
+
 from theaigame import TheAiGameParserDelegate, TheAiGameActionDelegate
 
 
