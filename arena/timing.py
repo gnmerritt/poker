aa8afcac9edf6db36223098622aeaff4e3a51ad6
Nathan Merritt <gnm@the-merritts.net>
2015-04-23 17:35:53 -0400

    new format timing

--- arena/timing.py
+++ arena/timing.py
@@ -3,8 +3,8 @@
 class ArenaTiming(object):
     def match_timing(self):
         """Timing information that's printed to the bots at match start"""
-        return ['Settings timeBank {tb}'.format(tb=self.time_bank),
-                'Settings timePerMove {tph}'.format(tph=self.time_per_hand), ]
+        return ['Settings time_bank {tb}'.format(tb=self.time_bank),
+                'Settings time_per_move {tph}'.format(tph=self.time_per_hand), ]
 
 
 class HalfSecondTurns(ArenaTiming):
